9a74763609d3b8f952fc74f281db0d3f37791a39
DaviMagalhaes <magalhaesp.davi@gmail.com>
2021-12-26 18:20:50 -0300

    Initial commit
    Display randomly generated map.
    
    Allow user to choose start node and end node through a GUI.
    
    Show A* solution.

---
+++ graph.py
@@ -0,0 +1,78 @@
+from collections import defaultdict, deque, namedtuple
+import random
+
+
+class Graph:
+    """
+    Classe de grafo baseado em lista de adjacencia.
+    O grafo e um defaultdict de dict,
+    no dict interior temos o vertice conectado com o valor do peso da aresta.
+    """
+
+    def __init__(self):
+        self.graph = defaultdict(dict)
+
+    def __len__(self):
+        return len(self.graph)
+
+    def __getitem__(self, key):
+        if key not in self.graph:
+            raise KeyError("Key {} not in the graph.".format(key))
+        return self.graph[key]
+
+    def __iter__(self):
+        return self.graph.items()
+
+    def __contains__(self, value):
+        return value in self.graph
+
+    def __str__(self):
+        return self.plotGraph()
+
+    def insertNode(self, node_id):
+        """
+        Insere um nó de id 'node_id' no grafo.
+        A operação é cancelada caso o nó já exista.
+        """
+        if self.searchNode(node_id):
+            return "ERROR! Node already in the graph"
+        else:
+            self.graph[node_id] = {}
+            return True
+
+    def add_edge(self, u, v, weight_=0, edgeID_=-1):
+        """
+        Conecta dois nos de id 'u' e 'v' atraves de uma arestas com um peso 'weight' associado.
+        Caso os nos nao existam, os mesmos sao criados.
+        O paramêtro 'inplace' especifica se, caso um dos nós não exista, os mesmos sejam criados.
+        """
+
+        if u not in self.graph:
+            self.graph[u] = {}
+
+        edge = namedtuple("Edge", ["edgeID", "weight"])
+        self.graph[u][v] = edge(edgeID=edgeID_, weight=weight_)
+
+        if v not in self.graph:
+            self.graph[v] = {}
+        self.graph[v][u] = edge(edgeID=edgeID_, weight=weight_)
+
+        return True
+
+
+    def plotGraph(self):
+        plot = "######################\n"
+        node_list = list(self.graph.items())
+        node_list.sort(key=lambda node: node[0].vertex_id)
+
+        for node in node_list:
+            plot += f"{node[0].vertex_id} -> "
+            for edge in node[1].items():
+                plot += f"({edge[0].vertex_id}, {edge[1].weight})"
+
+                if edge != list(node[1].items())[-1]:
+                    plot += ','
+
+            plot += "\n"
+        plot += "######################\n"
+        return plot
---
+++ main.py
@@ -0,0 +1,241 @@
+from random import randrange, choice
+import pygame
+import utils
+from collections import namedtuple, OrderedDict
+from graph import Graph
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
+import os
+import time
+
+
+bg_color = (255, 255, 255)
+node_color = (30, 144, 255)
+edge_color = (0, 0, 0)
+node_shape = (10, 10)
+grid_spacing = 50
+nb_nodes = 8
+bg_shape = (1300, 750)
+screen = pygame.display.set_mode(bg_shape)
+
+
+class GraphDraw(object):
+    def __init__(self):
+        self.nodes = set()
+
+    def add(self, node):
+        self.nodes.add(node)
+
+    def get_node(self, index):
+        for node in self.nodes:
+            if node.id == index:
+                return node
+        return None
+
+    def update(self):
+        screen.fill(bg_color)
+        for node in self.nodes:
+            pygame.draw.rect(screen, node.color, node.rect)
+            screen.blit(node.txt_surf, (node.rect.right + 10, node.rect.top))
+            for neighbor in node.neighbors:
+                pygame.draw.line(screen, edge_color, node.rect.center, neighbor.rect.center)
+
+
+class Node(object):
+    creation_counter = 0
+
+    def __init__(self, x, y, index):
+        # self.id = self.__class__.creation_counter
+        # self.__class__.creation_counter += 1
+
+        self.id = index
+        self.x = x
+        self.y = y
+
+        self.font = pygame.font.Font(None, 20)
+        self.font_color = (0, 0, 0)
+        self.rect = pygame.Rect(self.x, self.y, node_shape[0], node_shape[1])
+        # self.txt_surf = self.font.render(str(self.rect.topleft), True, self.font_color)
+        self.txt_surf = self.font.render(str(self.id), True, self.font_color)
+        self.color = node_color
+        self.neighbors = set()
+
+    def __hash__(self):
+        return self.id
+
+
+def search(nodes, index):
+    for node in nodes:
+        if node.id == index:
+            return True
+    return False
+
+
+def create_graph():
+    test = (30, 375, 0.2)
+    graph = Graph()
+    cities = list(range(test[0]))
+
+    test_map, most_far_nodes = utils.map_generator(cities, test[2], weights_range=(-1 * test[1], test[1]))
+    vertex = namedtuple("Vertex", ["vertex_id", "vertex_x", "vertex_y"])
+
+    graph_draw = GraphDraw()
+    nodes = []
+
+    for connection in test_map:
+        node1 = vertex(vertex_id=connection[0][0], vertex_x=connection[0][1][0], vertex_y=connection[0][1][1])
+        node2 = vertex(vertex_id=connection[1][0], vertex_x=connection[1][1][0], vertex_y=connection[1][1][1])
+        weight = connection[2]
+        graph.add_edge(node1, node2, weight)
+
+        if not search(nodes, index=node1):
+            a = Node(x=node1.vertex_x + test[1], y=node1.vertex_y + test[1], index=node1.vertex_id)
+            nodes.append(a)
+            graph_draw.add(a)
+        else:
+            for i in nodes:
+                if i.id == node1.vertex_id:
+                    a = i
+                    break
+        if not search(nodes, index=node2):
+            b = Node(x=node2.vertex_x + test[1], y=node2.vertex_y + test[1], index=node2.vertex_id)
+            nodes.append(b)
+            graph_draw.add(b)
+        else:
+            for i in nodes:
+                if i.id == node2.vertex_id:
+                    b = i
+                    break
+
+        a.neighbors.add(b)
+        b.neighbors.add(a)
+
+    return graph_draw, graph
+
+
+def a_star(graph, start_id, end_id, graph_draw):
+    expanded = 0
+    branching_factor = []
+
+    start_node = [node for node in list(graph.graph.keys()) if node.vertex_id == start_id][0]
+    end_node = [node for node in list(graph.graph.keys()) if node.vertex_id == end_id][0]
+
+
+    openList = {}
+    closedList = {}
+
+    # A lista contém respectivamente: g(custo acumulado), funcao h da heuristica e funcao f(g + h)
+    openList[start_node] = [0, utils.heuristic(
+        start_node, end_node), 0 + utils.heuristic(start_node, end_node)]
+
+    success = False
+    solution = []
+    parentMap = {}
+
+    while openList:
+        current = utils.find_smaller(openList, 'a_star')
+
+        current_draw = graph_draw.get_node(index=current.vertex_id)
+        pygame.draw.rect(screen, (124, 252, 0), current_draw.rect)
+        pygame.display.update()
+        time.sleep(1)
+
+        closedList[current] = openList[current]
+        del openList[current]
+        if current == end_node:
+            success = True
+            break
+        else:
+            expanded += 1
+            n = 0
+            for child in graph[current]:
+                if child in closedList:
+                    continue
+                if child in openList:
+                    new_g = closedList[current][0] + graph[current][child].weight
+                    if openList[child][0] > new_g:
+                        openList[child][0] = new_g
+                        openList[child][2] = new_g + openList[child][1]
+                        parentMap[child] = current
+                        n += 1
+                else:
+                    child_g = closedList[current][0] + graph[current][child].weight
+                    child_h = utils.heuristic(child, end_node)
+                    openList[child] = [child_g, child_h, child_g + child_h]
+                    parentMap[child] = current
+                    n += 1
+            branching_factor.append(n)
+
+    cost = 0
+    if success:
+        while current != start_node:
+            solution.append(current.vertex_id)
+            cost += graph[current][parentMap[current]].weight
+            current = parentMap[current]
+        solution.append(current.vertex_id)
+        solution.reverse()
+
+        depth = len(solution) - 1
+        average = sum(branching_factor) / len(branching_factor)
+
+        Tk().wm_withdraw()
+        result = messagebox.askokcancel('Program Finished',
+                                        ('The program finished, the shortest distance \n to the path is ' +
+                                         str("{:.2f}".format(cost)) +
+                                         ' blocks away, \n would you like to re run the program?'))
+        if result:
+            os.execl(sys.executable, sys.executable, *sys.argv)
+            pygame.quit()
+
+        return solution, depth, cost, expanded, len(openList) + len(closedList), average, 'success'
+    else:
+        return solution, -1, cost, expanded, len(openList), -1, 'failure'
+
+
+def onsubmit():
+    global start
+    global end
+    st = startBox.get()
+    ed = endBox.get()
+    start = int(st)
+    end = int(ed)
+    window.quit()
+    window.destroy()
+
+
+pygame.init()
+
+window = Tk()
+window.geometry('200x100')
+label0 = Label(window, text='Start id: ')
+# label0.place(x=0, y=100)
+startBox = Entry(window)
+# startBox.place(x=50, y=100)
+label1 = Label(window, text='End id: ')
+# label1.place(x=0, y=200)
+endBox = Entry(window)
+# endBox.place(x=50, y=200)
+submit = Button(window, text='Submit', command=onsubmit)
+# submit.place(x=50, y=300)
+window.update()
+
+# showPath.grid(columnspan=2, row=2)
+submit.grid(columnspan=2, row=3)
+label1.grid(row=1, pady=3)
+endBox.grid(row=1, column=1, pady=3)
+startBox.grid(row=0, column=1, pady=3)
+label0.grid(row=0, pady=3)
+
+graph_draw, graph = create_graph()
+
+while True:
+    ev = pygame.event.poll()
+    if ev.type == pygame.QUIT:
+        pygame.quit()
+    graph_draw.update()
+    pygame.display.update()
+    window.mainloop()
+    a_star(start_id=start, end_id=end, graph_draw=graph_draw, graph=graph)
+    # pygame.display.flip()
+    # pygame.time.delay(30)
